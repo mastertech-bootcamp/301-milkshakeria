608c7d0e82a216aa717799552c2c6db887348c9c
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-22 17:58:26 -0200

    Adiciona header e vitrine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProdutoComponent } from './produto/produto.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule
