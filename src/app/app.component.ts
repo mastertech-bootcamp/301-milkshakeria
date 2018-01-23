488c096412ce0a2c385050c9d6653125853a8ddf
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-23 13:40:44 -0200

    Interpolação, input e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  produtos = [
+    'https://www.completelydelicious.com/wp-content/uploads/2017/07/pie-milkshake-4-660x990.jpg',
+    'http://del.h-cdn.co/assets/15/49/640x960/gallery-1448988386-delish-milkshakes-eggnog-chocolate.jpg',
+    'https://www.browneyedbaker.com/wp-content/uploads/2012/01/white-russian-milkshake-1-550.jpg'
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
